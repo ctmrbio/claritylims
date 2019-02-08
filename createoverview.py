1ae675b862363ef8d8a6df68696c86fefe53b08c
Kim Wong <kimspindel@gmail.com>
2019-02-08 17:59:18 +0100

    Add initial createoverviewfile script (not finished)

---
+++ createoverview.py
@@ -0,0 +1,77 @@
+DESC = """Create an overview excel file on the Aggregate QC step""" 
+
+from argparse import ArgumentParser
+
+import genologics
+from genologics.lims import Lims
+from genologics.config import BASEURI, USERNAME, PASSWORD
+
+from genologics.entities import Process
+
+import logging
+import sys
+import xlwt
+
+def get_udf_if_exists(artifact, udf):
+    if (udf in artifact.udf):
+        return artifact.udf[udf]
+    else:
+        return ""
+
+def get_field_style(field, threshold):
+    style = xlwt.XFStyle()
+    if type(field) == int or type(field) == float:
+        if float(field) < threshold:
+            style = xlwt.Style.easyxf('pattern: pattern solid, fore_colour red;')
+    return style
+
+
+
+def main(lims, args, epp_logger):
+    p = Process(lims, id = args.pid)
+
+    new_workbook = xlwt.Workbook()
+    new_sheet = new_workbook.add_sheet('Sheet 1')
+    for col, heading in enumerate(["Sample name", "Container", "Well", "QuantIt HS Concentration", "QuantIt BR Concentration", "Qubit Concentration", "Chosen Concentration"]):
+        new_sheet.write(0, col, heading)
+    
+    for i, artifact in enumerate(p.all_inputs(unique=True)):
+        sample_name = "placeholder name"
+        container = "placeholder container"
+        well = "Z:99"
+    47          # Input Well, Input Container
+    48          i_w, i_c = i_a.location[1], i_a.location[0]
+        conc_hs = get_udf_if_exists(artifact, "QuantIt HS Concentration")
+        conc_br = get_udf_if_exists(artifact, "QuantIt BR Concentration")
+        conc_qb = get_udf_if_exists(artifact, "Qubit Concentration")
+        conc_chosen = get_udf_if_exists(artifact, "Concentration")
+        for col, field in enumerate([sample_name, container, well, conc_hs, conc_br, conc_qb, conc_chosen]):
+            style = get_field_style(field, float(args.threshold))
+            new_sheet.write(i + 1, col, field, style)
+
+    new_workbook.save(args.outputFilename)
+
+if __name__ == "__main__":
+    # Initialize parser with standard arguments and description
+    parser = ArgumentParser(description=DESC)
+    parser.add_argument('--pid',
+                        help='Lims id for current Process')
+    parser.add_argument('-f', '--outputFilename', required=True,
+                        help='lims ID of the new file')
+    parser.add_argument('--log', default=sys.stdout,
+                        help=('File name for standard log file, '
+                              'for runtime information and problems.'))
+    parser.add_argument('--aggregate', action='store_true',
+                        help=("Use this tag if your process is aggregating "
+                              "results. The default behaviour assumes it is "
+                              "the output artifact of type analyte that is "
+                              "modified while this tag changes this to using "
+                              "input artifacts instead"))
+    parser.add_argument('-t', '--threshold', default=4,
+                        help='threshold for red text')
+
+    args = parser.parse_args()
+
+    lims = Lims(BASEURI, USERNAME, PASSWORD)
+    lims.check_version()
+    main(lims, args, None)
