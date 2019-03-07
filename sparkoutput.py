9b935febbabcd2886770714cec040a99140b586c
Kim Wong <kimspindel@gmail.com>
2019-03-07 08:45:25 +0100

    Added convertToNm, fragmentSize, concentrationUdfNm to the sparkoutput script

--- sparkoutput.py
+++ sparkoutput.py
@@ -14,8 +14,19 @@ Usage:
     bash -c "python /opt/gls/clarity/customextensions/sparkoutput.py 
     --pid {processLuid}
     --sparkOutputFile 'Spark HighSens File'
-    --concentrationUdf 'QuantIt HS Concentration'"
+    --concentrationUdf 'QuantIt HS Concentration'
+   [--convertToNm
+    --fragmentSize '620bp'
+    --concentrationUdfNm 'QuantIt HS Concentration (nM)']
+    "
 """
+
+def format_fragment_size(fragment_size):
+    # can either be '620bp' or '620' or maybe even '620 bp'
+    match = re.search(r'([0-9]{2,4})\s*bp', fragment_size)
+    if match:
+        return float(match.group(1))
+    raise(RuntimeError("Invalid fragment size '%s'! Please specify the fragment size in the format '620' or '620bp'" % fragment_size))
 
 def get_spark_file(process, filename):
     content = None
@@ -58,6 +69,10 @@ def format_concentration(concentration):
         raise(RuntimeError("Error! Invalid concentration '%s' for well %s, row %s" % (concentration, well, row_i)))
     return concentration
 
+def convert_to_nm(concentration, fragment_size):
+    # convert from ng/ul to nM
+    return (concentration * 1538.4615) / fragment_size
+
 def main(lims, args, logger):
     p = Process(lims, id=args.pid)
     sparkfile = get_spark_file(p, args.sparkOutputFilename)
@@ -68,6 +83,8 @@ def main(lims, args, logger):
     sheet = workbook.sheet_by_index(0)
 
     well_re = re.compile("[A-Z][0-9]{1,2}")
+    if args.convertToNm:
+        fragment_size = format_fragment_size(args.fragmentSize)
 
     for row_i in range(0, sheet.nrows):
         if is_well(sheet.cell(row_i, 0).value, well_re):
@@ -81,15 +98,23 @@ def main(lims, args, logger):
                 concentration = sheet.cell(row_i, 1).value
             concentration = format_concentration(concentration)
 
+
             output = find_output_artifact(artifact.name, p)
             output.udf[args.concentrationUdf] = concentration
+            if args.convertToNm:
+                concentration_nm = convert_to_nm(concentration, fragment_size)
+                output.udf[args.concentrationUdfNm] = concentration_nm
             output.put()
 
 if __name__ == "__main__":
     parser = ArgumentParser(description=__doc__)
     parser.add_argument('--pid', required=True, help='LIMS ID for current Process')
-    parser.add_argument('--concentrationUdf', required=True, help='the concentration UDF to set')
+    parser.add_argument('--concentrationUdf', required=True, help='The concentration UDF to set')
     parser.add_argument('--sparkOutputFilename', required=True, help='LIMS name of the Spark file uploaded to the process')
+    # for setting an additional nM concentration UDF, e.g. 'QuantIt HS Concentration (nM)'
+    parser.add_argument('--convertToNm', default=False, action='store_true', help='Should the parsed concentrations be converted from ng/ul to nM or not?')
+    parser.add_argument('--fragmentSize', default='620bp', help='The average fragment size of the DNA, if converting to nM')
+    parser.add_argument('--concentrationUdfNm', help='The nM concentration UDF to set')
 
     args = parser.parse_args()
     lims = Lims(BASEURI, USERNAME, PASSWORD)
