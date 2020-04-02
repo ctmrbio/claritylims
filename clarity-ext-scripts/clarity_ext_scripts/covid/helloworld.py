53d39b76ddd5a03d3be3b865e996f9a4254a000f
Steinar Sturlaugsson <costeinar@gmail.com>
2020-04-02 15:32:43 +0200

    Hello world

---
+++ clarity-ext-scripts/clarity_ext_scripts/covid/helloworld.py
@@ -0,0 +1,14 @@
+from clarity_ext.extensions import GeneralExtension
+
+
+class Extension(GeneralExtension):
+    """
+    TODO: Add description
+    """
+    def execute(self):
+        # TODO: Add the code that should execute when you run the extension
+        pass
+
+    def integration_tests(self):
+        # TODO: Replace with a valid step ID
+        yield "24-1234"
