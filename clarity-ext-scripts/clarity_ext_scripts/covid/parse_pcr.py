48025a6e3cf8397460efaa0debe49daf38bb3884
Steinar Sturlaugsson <costeinar@gmail.com>
2020-04-08 13:09:31 +0200

    Use Ct (cyrillic t) instead of numeric index

--- clarity-ext-scripts/clarity_ext_scripts/covid/parse_pcr.py
+++ clarity-ext-scripts/clarity_ext_scripts/covid/parse_pcr.py
@@ -1,13 +1,12 @@
+# -*- coding: utf-8 -*-
+
 import pandas as pd
 from clarity_ext.extensions import GeneralExtension
 
-# NOTE: The file actually contains the cyrillic small letter te. To avoid using that for indexing
-# we instead use the location of the column (zero indexed, where the Well column is not a part of
-# the table)
+CT_HEADER = u"Cт"
 
 class Extension(GeneralExtension):
     def execute(self):
-        ct_col = 5
         file_name = "Result file"
         f = self.context.local_shared_file(file_name, mode="rb")
         data = pd.read_excel(f, 'Results', index_col=0, skiprows=7, encoding='utf-8')
@@ -21,7 +20,7 @@ class Extension(GeneralExtension):
                         "Expected {}".format(
                             target_name, output.well.alpha_num_key, file_name, output.name))
 
-            ct = entry[ct_col]
+            ct = entry[CT_HEADER]
             output.udf_map.force("CT", ct)
             self.context.update(output)
 
