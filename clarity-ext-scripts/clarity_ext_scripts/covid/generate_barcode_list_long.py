7c58dd539741a4fc9982894e8bd3ad17e433ee3a
Edvard Englund <edvard.englund@medsci.uu.se>
2020-04-09 11:09:57 +0200

    Print barcode labels including container names in barcode

--- clarity-ext-scripts/clarity_ext_scripts/covid/generate_barcode_list_long.py
+++ clarity-ext-scripts/clarity_ext_scripts/covid/generate_barcode_list_long.py
@@ -5,12 +5,12 @@ from clarity_ext_scripts.covid.label_printer import label_printer
 class Extension(GeneralExtension):
     """
     Generate a list of barcodes as zpl code for containers in step.
-    The barcode is here the lims-id of the containers
+    The barcode is here the name of the containers
     """
     def execute(self):
         file_name = 'printfile.zpl'
         containers = self.context.output_containers
-        label_printer.generate_zpl_for_containers(containers, lims_id_as_barcode=True)
+        label_printer.generate_zpl_for_containers(containers, lims_id_as_barcode=False)
         contents = label_printer.contents
         upload_packet = [(file_name, contents)]
         self.context.file_service.upload_files("Print files", upload_packet)
