8d3df3bf9ea4f32d69c5996dc6876c67899dc129
Edvard Englund <edvard.englund@medsci.uu.se>
2020-04-15 17:31:44 +0200

    Make CtDiscriminator testable

--- clarity-ext-scripts/clarity_ext_scripts/covid/ct_discriminator.py
+++ clarity-ext-scripts/clarity_ext_scripts/covid/ct_discriminator.py
@@ -1,64 +1,31 @@
 from clarity_ext.domain.validation import UsageError
 
 
-VALID_ASSAYS = ['Assay 1']
-
-BOUNDARY_DICT = {'Assay 1': (10, 50)}
-
-
-class CtDiscriminator(object):
+class CtDiscriminatorForPlate(object):
     """
     A service to analyze if a sample is positive for Covid19
     Given:
     CT value of sample
     Discriminator for assay (lower bound, upper bound)
-    This service does not update any artifact from context, it only reads from it
+    It's expected to be 2 controls on a plate, and these are to be
+    validated before ordinary samples are analyzed.
     """
-    def __init__(self, context):
-        self.context = context
-        self.initial_validation()
+    def __init__(self):
+        self.has_valid_pos_control = False
+        self.has_valid_neg_control = False
 
-    def initial_validation(self):
-        if not self._has_assay_udf():
-            raise UsageError("The udf 'Assay' must be filled in before running this script")
-        if self.assay not in VALID_ASSAYS:
-            raise UsageError("The current assay value is not recognized: {}"
-                             .format(self.assay))
+    def validate_control_value(self, control_type, ct_value, lower_bound, upper_bound):
+        if control_type.lower() == 'rtpcr_pos' and ct_value <= lower_bound:
+            self.has_valid_pos_control = True
+        if control_type.lower() == 'rtpcr_neg' and ct_value >= upper_bound:
+            self.has_valid_neg_control = True
 
-    @property
-    def assay(self):
-        return self.context.current_step.udf_assay
-
-    def _has_assay_udf(self):
-        try:
-            _ = self.assay
-        except AttributeError:
-            return False
-
-        return True
-
-    def _get_discriminator_values(self):
-        """
-        Returns: lower bound, upper bound
-        ct <= lower bound: counted as a positive value
-        ct >=upper bound: is counted as a negative value
-        lower bound < ct < upper bound: undefined
-        """
-        return BOUNDARY_DICT[self.assay]
-
-    def validate_control_value(self, control_type, ct_value):
-        lower_bound, upper_bound = self._get_discriminator_values()
-        if control_type.lower() == 'rtpcr_pos' and ct_value > lower_bound:
-            return False
-        if control_type.lower() == 'rtpcr_neg' and ct_value < upper_bound:
-            return False
-        return True
-
-    def analyze(self, ct_value):
+    def analyze(self, ct_value, lower_bound, upper_bound):
         """
         Returns: String with value of either 'positive', 'negative', 'failed'
         """
-        lower_bound, upper_bound = self._get_discriminator_values()
+        if not self.has_valid_pos_control or not self.has_valid_neg_control:
+            raise UsageError("There are no valid control samples")
         if ct_value <= lower_bound:
             return 'positive'
         elif ct_value >= upper_bound:
--- clarity-ext-scripts/clarity_ext_scripts/covid/rtpcr_analyes_service.py
+++ clarity-ext-scripts/clarity_ext_scripts/covid/rtpcr_analyes_service.py
@@ -1,6 +1,9 @@
-from clarity_ext.extensions import GeneralExtension
-from clarity_ext_scripts.covid.ct_discriminator import CtDiscriminator
+from clarity_ext_scripts.covid.ct_discriminator import CtDiscriminatorForPlate
 from clarity_ext.domain.validation import UsageError
+
+VALID_ASSAYS = ['Assay 1']
+
+BOUNDARY_DICT = {'Assay 1': (10, 50)}
 
 
 class RTPCRAnalyseService(object):
@@ -8,16 +11,23 @@ class RTPCRAnalyseService(object):
         self.context = context
 
     def execute(self):
-        ct_analysis_service = CtDiscriminator(self.context)
+        self.initial_validation()
+        ct_analysis_service = CtDiscriminatorForPlate()
+        lower_bound, upper_bound = BOUNDARY_DICT[self.assay]
 
         rt_pcr_control_types = ['rtpcr_pos', 'rtpcr_neg']
         found_controls = list()
+        self.context.current_step.udf_rtpcr_passed = False
+        for _, output in self.context.all_analytes:
+            original_sample = output.sample()
+            original_sample.udf_map.force("rtPCR Passed", False)
+
         # 1. Check control values
         for _, output in self.context.all_analytes:
             if output.udf_control == 'Yes' and output.udf_control_type.lower() in rt_pcr_control_types:
                 found_controls.append(output.udf_control_type.lower())
                 if not ct_analysis_service.validate_control_value(
-                        output.udf_control_type, output.udf_ct):
+                        output.udf_control_type, output.udf_ct, lower_bound, upper_bound):
                     return
 
         if set(rt_pcr_control_types) not in set(found_controls):
@@ -31,7 +41,26 @@ class RTPCRAnalyseService(object):
         for _, output in self.context.all_analytes:
             original_sample = output.sample()
             ct = output.udf_ct
-            covid_result = ct_analysis_service.analyze(ct)
+            covid_result = ct_analysis_service.analyze(ct, lower_bound, upper_bound)
             original_sample.udf_map.force("rtPCR covid-19 result", covid_result)
             original_sample.udf_map.force("rtPCR Passed", True)
             self.context.update(original_sample)
+
+    def initial_validation(self):
+        if not self._has_assay_udf():
+            raise UsageError("The udf 'Assay' must be filled in before running this script")
+        if self.assay not in VALID_ASSAYS:
+            raise UsageError("The current assay value is not recognized: {}"
+                             .format(self.assay))
+
+    @property
+    def assay(self):
+        return self.context.current_step.udf_assay
+
+    def _has_assay_udf(self):
+        try:
+            _ = self.assay
+        except AttributeError:
+            return False
+
+        return True
