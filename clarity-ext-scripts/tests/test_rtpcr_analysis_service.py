0af8e933271c93ba0149995f6386a562ce5d474d
Johan Dahlberg <johan@uppsala-bioinformatics.se>
2020-04-17 16:25:15 +0200

    Fixed test scenario 6 for analysis

--- clarity-ext-scripts/tests/test_rtpcr_analysis_service.py
+++ clarity-ext-scripts/tests/test_rtpcr_analysis_service.py
@@ -169,3 +169,35 @@ class TestRTPCRAnalysisService(object):
             {"id": "cov-3", "diagnosis_result": FAILED_ENTIRE_PLATE_BY_FAILED_EXTERNAL_CONTROL}]
 
         assert expected == list(result)
+
+    def test_can_analyze_samples_scenario6(self):
+
+        pos_controls = [{"id": "pos-1", "FAM-CT": 25, "HEX-CT": 20},
+                        {"id": "pos-2", "FAM-CT": 20, "HEX-CT": 18}]
+
+        neg_controls = [{"id": "neg-1", "FAM-CT": 0, "HEX-CT": 20},
+                        {"id": "neg-2", "FAM-CT": 40, "HEX-CT": 18}]
+
+        samples = [{"id": "cov-1", "FAM-CT": 20, "HEX-CT": 20},
+                   {"id": "cov-2", "FAM-CT": 0, "HEX-CT": 18},
+                   {"id": "cov-3", "FAM-CT": 0, "HEX-CT": 33}]
+
+        service = RTPCRAnalysisService(
+            covid_reporter_key="FAM", internal_control_reporter_key="HEX")
+
+        result = service.analyze_samples(positive_controls=pos_controls,
+                                         negative_controls=neg_controls,
+                                         samples=samples)
+
+        expected = [
+            {"id": "pos-1", "diagnosis_result": COVID_RESPONSE_POSITIVE},
+            {"id": "pos-2", "diagnosis_result": COVID_RESPONSE_POSITIVE},
+            {"id": "neg-1", "diagnosis_result": COVID_RESPONSE_NEGATIVE},
+            {"id": "neg-2", "diagnosis_result": FAILED_BY_TO_HIGH_COVID_VALUE},
+            {"id": "cov-1",
+                "diagnosis_result": FAILED_ENTIRE_PLATE_BY_FAILED_EXTERNAL_CONTROL},
+            {"id": "cov-2",
+                "diagnosis_result": FAILED_ENTIRE_PLATE_BY_FAILED_EXTERNAL_CONTROL},
+            {"id": "cov-3", "diagnosis_result": FAILED_ENTIRE_PLATE_BY_FAILED_EXTERNAL_CONTROL}]
+
+        assert expected == list(result)
