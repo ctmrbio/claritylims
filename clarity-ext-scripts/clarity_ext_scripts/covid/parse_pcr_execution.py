d053367638a845c92eb8f24765d9a3474258fac4
Edvard Englund <edvard.englund@medsci.uu.se>
2020-04-21 08:58:59 +0200

    Choose parser implementation based on udf value

--- clarity-ext-scripts/clarity_ext_scripts/covid/parse_pcr_execution.py
+++ clarity-ext-scripts/clarity_ext_scripts/covid/parse_pcr_execution.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 from datetime import datetime
+from clarity_ext.domain.validation import UsageError
 
 CT_HEADER = u"Cт"
 
@@ -11,10 +12,42 @@ class ParsePcrExecution(object):
         self.context = context
 
     def execute(self):
-        file_name = "Result file"
-        f = self.context.local_shared_file(file_name, mode="rb")
-        data = pd.read_excel(f, 'Results', index_col=0, skiprows=7, encoding='utf-8')
+        if not self._has_instrument_udf():
+            raise UsageError("The udf 'Instrument Used' must be filled in before running this script")
+        file_handle = "Result file"
+        parser = self._instantiate_parser()
+        parser.parse(file_handle)
 
+    def _instantiate_parser(self):
+        if self.instrument == 'qPCR ABI 7500':
+            raise UsageError('Parsing qPCR ABI 7500 is not implemented')
+        elif self.instrument == 'Quant Studio 7':
+            parser = Quant7Parser(self.context)
+        else:
+            raise UsageError("The instrument in 'Instrument Used' is not recognized: {}"
+                             .format(self.instrument))
+        return parser
+
+    @property
+    def instrument(self):
+        return self.context.current_step.udf_instrument_used
+
+    def _has_instrument_udf(self):
+        try:
+            _ = self.instrument
+        except AttributeError:
+            return False
+
+        return True
+
+
+class Quant7Parser(object):
+    def __init__(self, context):
+        self.context = context
+
+    def parse(self, file_name):
+        file_stream = self.context.local_shared_file(file_name, mode="rb")
+        data = pd.read_excel(file_stream, 'Results', index_col=0, skiprows=7, encoding='utf-8')
         for _, output in self.context.all_analytes:
             entry = data.loc[output.well.alpha_num_key]
             target_name = entry["Target Name"]
