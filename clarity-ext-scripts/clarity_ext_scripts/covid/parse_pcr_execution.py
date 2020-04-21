1f9f26644c48ca075b94e1411bd26408ce8d932e
Edvard Englund <edvard.englund@medsci.uu.se>
2020-04-21 14:37:45 +0200

    Adjust parse pcr script to Quant 7 output file
    parse both fam-ct and vic-ct

--- clarity-ext-scripts/clarity_ext_scripts/covid/parse_pcr_execution.py
+++ clarity-ext-scripts/clarity_ext_scripts/covid/parse_pcr_execution.py
@@ -4,7 +4,7 @@ import pandas as pd
 from datetime import datetime
 from clarity_ext.domain.validation import UsageError
 
-CT_HEADER = u"Cт"
+CT_HEADER_OLD = u"Cт"
 
 
 class ParsePcrExecution(object):
@@ -45,22 +45,32 @@ class Quant7Parser(object):
     def __init__(self, context):
         self.context = context
 
-    def parse(self, file_name):
-        file_stream = self.context.local_shared_file(file_name, mode="rb")
-        data = pd.read_excel(file_stream, 'Results', index_col=0, skiprows=7, encoding='utf-8')
-        for _, output in self.context.all_analytes:
-            entry = data.loc[output.well.alpha_num_key]
-            target_name = entry["Target Name"]
+    def _determine_start_row(self, file_handle):
+        file_stream = self.context.local_shared_file(file_handle, mode="rb")
+        data = pd.read_excel(file_stream, 'Results', index_col=None, encoding='utf-8')
+        header_row_entry = data[(data[0] == 'Well') & (data[1] == 'Well Position')].index
+        header_row_index = header_row_entry.values[0]
+        return header_row_index
 
+    def parse(self, file_handle):
+        header_row_index = self._determine_start_row(file_handle)
+        file_stream = self.context.local_shared_file(file_handle, mode="rb")
+        data = pd.read_excel(file_stream, 'Results', index_col=None, skiprows=header_row_index, encoding='utf-8')
+        for _, output in self.context.all_analytes:
+            fam_row = data.loc[(data['Sample Name'] == 'sample1') & (data['Reporter'] == 'FAM')]
+            vic_row = data.loc[(data['Sample Name'] == 'sample1') & (data['Reporter'] == 'VIC')]
+            target_name = fam_row["Sample Name"]
             if target_name != output.name:
                 raise AssertionError("Incorrect name of target '{}' in well '{}' in '{}'. "
                         "Expected {}".format(
-                            target_name, output.well.alpha_num_key, file_name, output.name))
+                            target_name, output.well.alpha_num_key, file_handle, output.name))
 
-            ct = entry[CT_HEADER]
+            fam_ct = fam_row["CT"]
+            vic_ct = vic_row["CT"]
 
             # add the measurement to the output artifact
-            output.udf_map.force("CT", ct)
+            output.udf_map.force("FAM-CT", fam_ct)
+            output.udf_map.force("VIC-CT", vic_ct)
             self.context.update(output)
 
             # also add the measurement to the original sample, where it should be understood
@@ -70,7 +80,8 @@ class Quant7Parser(object):
             now_iso = datetime.now().isoformat().split(".")[0]
 
             original_sample = output.sample()
-            original_sample.udf_map.force("CT latest", ct)
+            original_sample.udf_map.force("FAM-CT latest", fam_ct)
+            original_sample.udf_map.force("VIC-CT latest", vic_ct)
             original_sample.udf_map.force("CT latest date", now_iso)
             # LIMS ID of the source of the CT measurement
             original_sample.udf_map.force("CT source", output.api_resource.uri)
