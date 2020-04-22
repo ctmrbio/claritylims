e8c0b226a4dd1a3d71ed67ec61664526e3dbf5ca
Ingvar Thorsteinsson <ingvar.thorsteinsson@medsci.uu.se>
2020-04-22 09:14:30 +0200

    Generate the following files
    - input file for Quant studio7
    - example file abi 7500
    - example file Quant studion 7
    - TODO: Fix random numbers for controls in QS7

--- clarity-ext-scripts/clarity_ext_scripts/covid/pcr/input_file_rt-pcr.py
+++ clarity-ext-scripts/clarity_ext_scripts/covid/pcr/input_file_rt-pcr.py
@@ -1,5 +1,4 @@
 from clarity_ext.extensions import TemplateExtension
-from clarity_ext.domain.validation import UsageError
 from clarity_ext.domain import Container
 import datetime
 
@@ -17,18 +16,14 @@ class Extension(TemplateExtension):
     def filename(self):
         timestamp = datetime.datetime.now().strftime("%y%m%dT%H%M%S")
         container_name = self.context.output_container.name
-        user = self.context.current_user
-        return "{}_input_file_{}_{}.txt".format(container_name, user.initials, timestamp)
+        return "{}_QuantStudio-7_{}.txt".format(container_name, timestamp)
 
     def artifacts(self):
         max_number_of_plates = 1  # TODO: decide max number, perhaps it is just 1 anyway?
         if len(self.context.output_containers) > max_number_of_plates:
-            raise UsageError('The allowed max number of plates is currently set to {},'
-                             ' please contact system administrator'.format(max_number_of_plates))
+            raise Exception("The allowed max number of plates is currently set to {}".format(max_number_of_plates))
         for well in self.context.output_container.list_wells(Container.DOWN_FIRST):
-            if well.artifact is not None:
-                artifact = well.artifact
-                yield artifact
+            yield well
 
     def integration_tests(self):
-        yield self.test("24-43214", commit=False)
+        yield self.test("24-43779", commit=False)
