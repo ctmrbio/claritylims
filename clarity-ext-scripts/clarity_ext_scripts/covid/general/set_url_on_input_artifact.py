b5784332f932bbe358c290878f68879e2d1b965d
Johan Dahlberg <johan@uppsala-bioinformatics.se>
2020-04-29 13:38:46 +0200

    Set url properly on server, and remote machine

--- clarity-ext-scripts/clarity_ext_scripts/covid/general/set_url_on_input_artifact.py
+++ clarity-ext-scripts/clarity_ext_scripts/covid/general/set_url_on_input_artifact.py
@@ -1,32 +1,23 @@
-from clarity_ext.extensions import GeneralExtension
-import urlparse
+
 import socket
+
+from clarity_ext.extensions import GeneralExtension
+from genologics.config import BASEURI
 
 
 class Extension(GeneralExtension):
+
     def execute(self):
         for artifact in self.context.artifact_service.all_input_artifacts():
-
             _, step_id = artifact.parent_process.id.split("-", 1)
             path_to_step = "/clarity/work-complete/" + step_id
-            
-            url = artifact.parent_process.uri
-            host_name = socket.getfqdn()
-            if host_name == "c1-ctmr-lims-stage01.ki.se":
-                server_domain = "ctmr-lims-stage.scilifelab.se"
-            elif host_name == "c1-ctmr-lims-prod01.ki.se":
-                server_domain = "ctmr-lims-prod.scilifelab.se"
+            if "localhost" in BASEURI:
+                domain = socket.getfqdn()
             else:
-                server_domain = urlparse.urlparse(url).netloc
-
-            parse_object = urlparse.urlparse(url)
-            parse_object = parse_object._replace(netloc=server_domain, path=path_to_step)
-
-            url = parse_object.geturl()
-
-            artifact.udf_map.force("Link to RT-PCR Step", url)
-
+                domain = BASEURI
+            clarity_full_path = domain + path_to_step
+            artifact.udf_map.force("Link to RT-PCR Step", clarity_full_path)
             self.context.update(artifact)
 
     def integration_tests(self):
-        yield self.test("24-45334", commit=False)
+        yield self.test("24-44020", commit=False)
