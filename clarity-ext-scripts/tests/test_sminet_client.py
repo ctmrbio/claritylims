eccdc3619f86f8701b94aac290028e7276395700
Steinar Sturlaugsson <costeinar@gmail.com>
2020-05-19 12:53:50 +0200

    Minor bug fix

--- clarity-ext-scripts/tests/test_sminet_client.py
+++ clarity-ext-scripts/tests/test_sminet_client.py
@@ -2,14 +2,14 @@ from datetime import datetime
 from clarity_ext_scripts.covid.sminet_client import (create_covid_request, SampleInfo,
                                                      ReferringClinic, Patient, Doctor)
 import lxml.etree as ET
-mport os
+import os
 
 
 def test_can_create_expected_request():
     constant_date = datetime(2020, 4, 30)
     request_created = datetime(2020, 5, 18, 18, 11, 6)
 
-    sample_info = SampleInfo(status=1, sample_number=123, sample_date_arrival=constant_date,
+    sample_info = SampleInfo(status=1, sample_id="123", sample_date_arrival=constant_date,
                              sample_date_referral=constant_date, sample_material="Svalg",
                              sample_free_text_referral="Anamnes: Personalprov")
     clinic = ReferringClinic("Clinic name", "", "C", Doctor("Some doctor"))
