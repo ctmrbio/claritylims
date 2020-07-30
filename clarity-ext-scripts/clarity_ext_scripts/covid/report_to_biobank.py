7618e266042644a12dedbc727d79ddac85418999
chichackles <chekmen@gmail.com>
2020-07-30 11:42:12 +0200

    investigating UDFs

--- clarity-ext-scripts/clarity_ext_scripts/covid/report_to_biobank.py
+++ clarity-ext-scripts/clarity_ext_scripts/covid/report_to_biobank.py
@@ -14,10 +14,12 @@ class Extension(GeneralExtension):
 
     def report(self, sample):
         pp = pprint.PrettyPrinter()
-        pp.pprint(sample.id)
-        pp.pprint(sample.name)
-        pp.pprint(vars(sample.project))
+        pp.pprint(sample)
+#        pp.pprint(sample.id)
+#        pp.pprint(sample.name)
+#        pp.pprint(sample.project.name)
         pp.pprint(sample.udf_map)
+        pp.pprint(sample.api_resource)
 #        logger.info("Report2bioBank: sample: {}".format(pp.pformat(sample.project)))
 
     def execute(self):
@@ -26,12 +28,12 @@ class Extension(GeneralExtension):
             pp.pprint("Plate {}: {}".format(plate.id, plate.name))
             for well in plate.occupied:
                 pp.pprint("Well {}: {}".format(well.artifact.well, well.artifact.id))
-#                pp.pprint(well.artifact)
+#                pp.pprint(vars(well.artifact))
                 substance = CtmrCovidSubstanceInfo(well.artifact)
-#                pp.pprint(substance.substance)
+#                pp.pprint(vars(substance.substance))
                 sample = substance.submitted_sample
-#                pp.pprint(vars(sample))
+#                pp.pprint(sample)
                 self.report(sample)
 
     def integration_tests(self):
-        yield "24-51976"
+        yield "24-51996"
