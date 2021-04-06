7591e87062229d4097eb98406656fc537fc3752e
Fredrik Boulund <fredrik.boulund@gmail.com>
2021-04-06 08:54:01 +0200

    Rename tube containing pool to be identical to pool name

--- clarity-ext-scripts/clarity_ext_scripts/covid_seq/atoplex/validate_pools.py
+++ clarity-ext-scripts/clarity_ext_scripts/covid_seq/atoplex/validate_pools.py
@@ -11,14 +11,18 @@ class Extension(GeneralExtension):
         start = self.context.start
         date = start.strftime("%Y%m%d")
 
-        for idx, pool in enumerate(set(self._all_outputs)):
+        for idx, pool in enumerate(set(self._all_outputs), start=1):
             used_adapters = [s.udf_adapter_id_forward for s in pool.samples]
             if len(used_adapters) != len(set(used_adapters)):
                 self.usage_error("Cannot combine pools with overlapping adapters!")
 
             pool_member_plates = set([s.udf_biobank_plate_id for s in pool.samples])
-            pool.name = "{}_{}_{}".format(date, "_".join(pool_member_plates), str(idx+1))
+            pool.name = "{}_{}_{}".format(date, "_".join(pool_member_plates), str(idx))
             self.context.update(pool)
+
+            out_container = pool.container
+            out_container.name = pool.name
+            self.context.update(out_container)
 
     @property
     def _all_outputs(self):
