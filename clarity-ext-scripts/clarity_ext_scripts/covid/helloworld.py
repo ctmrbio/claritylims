b6fb6cf7f938e223fa9e7b3d7b965d0be5f42006
Fredrik Boulund <fredrik.boulund@gmail.com>
2021-04-08 10:59:19 +0200

    Fix helloword.py

--- clarity-ext-scripts/clarity_ext_scripts/covid/helloworld.py
+++ clarity-ext-scripts/clarity_ext_scripts/covid/helloworld.py
@@ -6,9 +6,9 @@ class Extension(GeneralExtension):
     A helloworld extension!
     """
     def execute(self):
-        print(self.config['covid.test_partner_url'],
-              self.config['covid.test_partner_user'],
-              self.config['covid.test_partner_password'])
+        print(self.config['test_partner_url'],
+              self.config['test_partner_user'],
+              self.config['test_partner_password'])
 
     def integration_tests(self):
         yield "24-38707"
